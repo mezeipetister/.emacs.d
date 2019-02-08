4815f6e5e007d59733f949107577f15d9490f4e0
Peter Mezei <mezeipetister@gmail.com>
2019-02-08 16:03:32 +0100

    init.el added

---
+++ init.el
@@ -0,0 +1,12 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
