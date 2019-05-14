5afedaf82a84834888f0d2495ecff5c1fe25b3d9
Peter Mezei <mezeipetister@gmail.com>
2019-05-14 07:16:11 +0200

    mu4e context settings now maybe works fine, test should be done

--- mu4e4.el
+++ mu4e4.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
 ;;(add-to-list 'load-path "~/.emacs.d/vendor/mu4e-multi")
 (require 'mu4e)
 (require 'smtpmail)
@@ -19,54 +19,81 @@
       mu4e-view-image-max-width 100
       starttls-use-gnutls t)
 
-(defvar my-mu4e-account-alist
-  '(("Gmail"
-     (mu4e-drafts-folder "/Gmail/[Gmail].Drafts")
-     (mu4e-sent-folder   "/Gmail/[Gmail].Sent Mail")
-     (mu4e-trash-folder  "/Gmail/[Gmail].Trash")
-     (mu4e-refile-folder "/Gmail/[Gmail].All Mail")
-     (user-mail-address "mezeipetister@gmail.com")
-     (user-full-name "Peter Mezei")
-     (mu4e-compose-signature (concat
-      "Peter Mezei\n"
-      ""))
-     (smtpmail-smtp-user "mezeipetister")
-     (smtpmail-default-smtp-server "smtp.gmail.com")
-     (smtpmail-smtp-server "smtp.gmail.com")
-     (smtpmail-smtp-service 587))
-    ("Gardenova"
-     (mu4e-drafts-folder "/Gardenova/[Gmail].Drafts")
-     (mu4e-sent-folder   "/Gardenova/[Gmail].Sent Mail")
-     (mu4e-trash-folder  "/Gardenova/[Gmail].Trash")
-     (mu4e-refile-folder "/Gardenova/[Gmail].All Mail")
-     (user-mail-address "peter.mezei@gardenova.hu")
-     (user-full-name "Peter Mezei")
-     (mu4e-compose-signature (concat
-      "Peter Mezei\n"
-      "Gardenova"))
-     (smtpmail-smtp-user "peter.mezei@gardenova.hu")
-     (smtpmail-default-smtp-server "smtp.gmail.com")
-     (smtpmail-smtp-server "smtp.gmail.com")
-     (smtpmail-smtp-service 587)
-     )))
+(setq mu4e-contexts
+      `( ,(make-mu4e-context
+	  :name "private"
+	  :enter-func (lambda () (mu4e-message "Entering Gmail context"))
+	  :leave-func (lambda () (mu4e-message "Leaving Gmail context"))
+	  :match-func (lambda (msg)
+			(when msg
+			  (string-match-p "^/Gmail" (mu4e-message-field msg :maildir))))
+	  :vars '((user-mail-address	. "mezeipetister@gmail.com")
+		  (mu4e-drafts-folder	. "/Gmail/[Gmail].Drafts")
+		  (mu4e-sent-folder	. "/Gmail/[Gmail].Sent Mail")
+		  (mu4e-trash-folder	. "/Gmail/[Gmail].Trash")
+		  (mu4e-refile-folder	. "/Gmail/[Gmail].All Mail")))
+	,(make-mu4e-context
+	 :name "work"
+	 :enter-func (lambda () (mu4e-message "Entering Gardenova context"))
+	 :leave-func (lambda () (mu4e-message "Leaving Gardenova context"))
+	 :match-func (lambda (msg)
+			(when msg
+			  (string-match-p "^/Gardenova" (mu4e-message-field msg :maildir))))
+	 :vars '((user-mail-address	. "peter.mezei@gardenova.hu")
+		 (mu4e-drafts-folder	. "/Gardenova/[Gmail].Drafts")
+		 (mu4e-sent-folder	. "/Gardenova/[Gmail].Sent Mail")
+		 (mu4e-trash-folder	. "/Gardenova/[Gmail].Trash")
+		 (mu4e-refile-folder	. "/Gardenova/[Gmail].All Mail"))
+	 )))
 
-(defun my-mu4e-set-account ()
-  "Set the account for composing a message."
-  (let* ((account
-          (if mu4e-compose-parent-message
-              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-                (string-match "/\\(.*?\\)/" maildir)
-                (match-string 1 maildir))
-            (completing-read (format "Compose with account: (%s) "
-                                     (mapconcat #'(lambda (var) (car var))
-                                                my-mu4e-account-alist "/"))
-                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-                             nil t nil nil (caar my-mu4e-account-alist))))
-         (account-vars (cdr (assoc account my-mu4e-account-alist))))
-    (if account-vars
-        (mapc #'(lambda (var)
-                  (set (car var) (cadr var)))
-              account-vars)
-      (error "No email account found"))))
+;; (defvar my-mu4e-account-alist
+;;   '(("Gmail"
+;;      (mu4e-drafts-folder "/Gmail/[Gmail].Drafts")
+;;      (mu4e-sent-folder   "/Gmail/[Gmail].Sent Mail")
+;;      (mu4e-trash-folder  "/Gmail/[Gmail].Trash")
+;;      (mu4e-refile-folder "/Gmail/[Gmail].All Mail")
+;;      (user-mail-address "mezeipetister@gmail.com")
+;;      (user-full-name "Peter Mezei")
+;;      (mu4e-compose-signature (concat
+;;       "Peter Mezei\n"
+;;       ""))
+;;      (smtpmail-smtp-user "mezeipetister")
+;;      (smtpmail-default-smtp-server "smtp.gmail.com")
+;;      (smtpmail-smtp-server "smtp.gmail.com")
+;;      (smtpmail-smtp-service 587))
+;;     ("Gardenova"
+;;      (mu4e-drafts-folder "/Gardenova/[Gmail].Drafts")
+;;      (mu4e-sent-folder   "/Gardenova/[Gmail].Sent Mail")
+;;      (mu4e-trash-folder  "/Gardenova/[Gmail].Trash")
+;;      (mu4e-refile-folder "/Gardenova/[Gmail].All Mail")
+;;      (user-mail-address "peter.mezei@gardenova.hu")
+;;      (user-full-name "Peter Mezei")
+;;      (mu4e-compose-signature (concat
+;;       "Peter Mezei\n"
+;;       "Gardenova"))
+;;      (smtpmail-smtp-user "peter.mezei@gardenova.hu")
+;;      (smtpmail-default-smtp-server "smtp.gmail.com")
+;;      (smtpmail-smtp-server "smtp.gmail.com")
+;;      (smtpmail-smtp-service 587)
+;;      )))
 
-(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+;; (defun my-mu4e-set-account ()
+;;   "Set the account for composing a message."
+;;   (let* ((account
+;;           (if mu4e-compose-parent-message
+;;               (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+;;                 (string-match "/\\(.*?\\)/" maildir)
+;;                 (match-string 1 maildir))
+;;             (completing-read (format "Compose with account: (%s) "
+;;                                      (mapconcat #'(lambda (var) (car var))
+;;                                                 my-mu4e-account-alist "/"))
+;;                              (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+;;                              nil t nil nil (caar my-mu4e-account-alist))))
+;;          (account-vars (cdr (assoc account my-mu4e-account-alist))))
+;;     (if account-vars
+;;         (mapc #'(lambda (var)
+;;                   (set (car var) (cadr var)))
+;;               account-vars)
+;;       (error "No email account found"))))
+
+;; (add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
