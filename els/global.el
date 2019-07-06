3252b335c8d13e72aef82e2eb2f11dceb812618a
Peter Mezei <mezeipetister@gmail.com>
2019-07-06 11:13:42 +0200

    keyboard shortcut line up/down

--- els/global.el
+++ els/global.el
@@ -11,3 +11,34 @@
 
 (setq-default fill-column 80)
 (setq company-tooltip-align-annotations t)
+
+;; (require 'unicode-fonts)
+;; (unicode-fonts-setup)
+
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+;; -------------------------------
+;; | Line MOVE UP/DOWN           | 
+;; | + Keyboard shortcut         |
+;; -------------------------------
+
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key [(control shift down)]  'move-line-down)
+(global-set-key [(control shift up)]  'move-line-up)
+
+;; END
